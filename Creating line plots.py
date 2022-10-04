18885f7e4db32df2228028f412c82b88e280633d
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-04 21:28:52 +0100

    Update Creating line plots.py

--- Creating line plots.py
+++ Creating line plots.py
@@ -1,7 +1,20 @@
 import pandas as pd
-from
+from matplotlib import pyplot as plt
 
-data = {'day_of_week': ['M', 'Tu', 'W', 'Th', 'F'],
+deshaun = {'day_of_week': ['M', 'Tu', 'W', 'Th', 'F'],
         'hours_worked': [8, 5, 3, 5, 8]}
-deshaun = pd.DataFrame(data)
-print(deshaun)
+deshaun = pd.DataFrame(deshaun)
+
+mengfei = {'day_of_week': ['M', 'Tu', 'W', 'Th', 'F'],
+        'hours_worked': [0, 0, 5, 9, 5]}
+mengfei = pd.DataFrame(mengfei)
+
+aditya = {'day_of_week': ['M', 'Tu', 'W', 'Th', 'F'],
+        'hours_worked': [10, 2, 1, 0, 0]}
+aditya = pd.DataFrame(aditya)
+
+plt.plot(deshaun.day_of_week, deshaun.hours_worked)
+plt.plot(mengfei.day_of_week, mengfei.hours_worked)
+plt.plot(aditya.day_of_week, aditya.hours_worked)
+
+plt.show()
